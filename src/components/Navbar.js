edd3c48df1be5b75a5760fde5f56621b876e059f
rifky <ee.guy.42@gmail.com>
2021-12-27 18:57:31 +0700

    first ver

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import './Navbar.css'
+import profilelogo from '../icons/person.svg';
+import explogo from '../icons/journal-text.svg';
+import projectslogo from '../icons/boxes.svg';
+import contactslogo from '../icons/at.svg';
+
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <div className='navbar-container'>
+            <img src={profilelogo} className='profilelogo' alt='profile logo'/>
+            <img src={explogo} className='explogo' alt='experience logo'/>
+            <img src={projectslogo} className='projectslogo' alt='projects logo'/>
+            <img src={contactslogo} className='contactslogo' alt='contacts logo'/>
+        </div>
+    )
+}
---
+++ src/profile/Profile.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import githublogo from '../icons/github.svg';
+import emaillogo from '../icons/envelope.svg';
+import resumelogo from '../icons/journal-text.svg';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
+import Typical from 'react-typical';
+import './Profile.css'
+
+export default function Profile() {
+    return (
+        <div className='profile-container'>
+            <div className='profile-parent'>
+                <div className='profile-details'>
+                    <div className='cols'>
+                        <div className='profile-details-name'>
+                            <span className='primarytext'>
+                                {" "}
+                                Hello there.
+                            </span>
+                            <br></br>
+                        </div>
+                        <div className='profile-details-role'>
+                            <span className='actualname'>
+                                {" "}
+                                My name is Rifky Ariya.
+                            </span>
+                            <br></br>
+                            <span className='primarytext'>
+                                I am <Typical
+                                loop={Infinity}
+                                wrapper="b"
+                                steps={[
+                                    "A Web Developer(React)",
+                                    1000,
+                                    "An Article Writer",
+                                    1000,
+                                    "A Coding Teacher",
+                                    1000,
+                                    "An English Teacher",
+                                    1000,
+                                    "A Junior Programmer",
+                                    1000,
+                                    "A Cybersecurity Enthusiast",
+                                    1000,
+                                    "An Imperfect Being",
+                                    250,]}
+                                    />
+                            </span>
+                        </div>
+                        <div className='profile-details-etc'>
+                            ... and this is my story!
+                        </div>
+                        <a href='https://mail.google.com/mail/?view=cm&fs=1&to=ee.guy.42@gmail.com'>
+                        <img src={emaillogo} alt='emaillogo'/>
+                        </a> {" "}
+                        <a href='https://github.com/rif42?tab=repositories'>
+                            <img src={githublogo} alt='githublogo'/>
+                        </a> {" "}
+                        <a href='https://github.com/rif42?tab=repositories'>
+                            <img src={resumelogo} alt='githublogo'/>
+                        </a>                        
+                    </div>
+                </div>
+            </div>
+            <img id='bgimage' src='https://cdn.discordapp.com/attachments/922536453731418153/922539097548009532/ducky.png' alt='githublogo'/> 
+        </div>
+        
+    )
+}
