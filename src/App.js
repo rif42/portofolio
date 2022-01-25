ef175b3a105cf8bfaf84105f5196e9bf6a15ca2a
rifky <ee.guy.42@gmail.com>
2022-01-26 00:14:52 +0700

    navbar is ok. now put some effects on the navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 import Profile from './profile/Profile'
 import Projects from './projects/Projects.js';
 import WorkExperience from './work experience/WorkExperience.js';
+import Contacts from './contacts/Contacts';
 import profilelogo from './icons/person.svg';
 import explogo from './icons/journal-text.svg';
 import projectslogo from './icons/boxes.svg';
@@ -13,17 +14,18 @@ function App() {
     <Router>
     <div className="App">
       <div className='navbar-container'>
-        <img src={profilelogo} className='profilelogo' alt='profile logo'/>
-        <img src={explogo} className='explogo' alt='experience logo'/>
-        <img src={projectslogo} className='projectslogo' alt='projects logo'/>
-        <img src={contactslogo} className='contactslogo' alt='contacts logo'/>
+        <Link to='/'><img src={profilelogo} className='profilelogo' alt='profile logo'/></Link>
+        <Link to='/WorkExperience'><img src={explogo} className='explogo' alt='experience logo'/></Link>
+        <Link to='/Projects'><img src={projectslogo} className='projectslogo' alt='projects logo'/></Link>
+        <Link to='/Contacts'><img src={contactslogo} className='contactslogo' alt='contacts logo'/></Link>
       </div>
       {/* <Profile/> */}
     </div>
     <Routes>
       <Route path='/' element={<Profile/>}/>
-      <Route path='/Projects' element={<Projects/>}/>
       <Route path='/WorkExperience' element={<WorkExperience/>}/>
+      <Route path='/Projects' element={<Projects/>}/>
+      <Route path='/Contacts' element={<Contacts/>}/>
     </Routes>
     </Router>
   );
